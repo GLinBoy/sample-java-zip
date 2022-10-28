3170992a786971d3bfa5388a1f0296dfc9be9560
GLinBoy <hojjatabedie@gmail.com>
2022-10-28 14:08:26 +0200

    📦 NEW: Setup the structure of the project with the Maven

---
+++ src/main/java/com/glinboy/sample/zipper/App.java
@@ -0,0 +1,13 @@
+package com.glinboy.sample.zipper;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
